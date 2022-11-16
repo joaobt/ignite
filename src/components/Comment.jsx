4793a3b676cbcbe3b3575fe596773cfc7e3e94a0
joao <joao4729@gmail.com>
2022-11-16 17:45:59 -0300

    commt

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,11 +1,20 @@
 import styles from './comment.module.css'
 import { ThumbsUp, Trash } from 'phosphor-react'
 import { Avatar } from './Avatar'
+import { useState } from 'react'
 
 
 export function Comment({content, onDeleteComment}) {
+    const [likeCount, setLikeCount] = useState(0)
     function handleDeleteComment() {
         onDeleteComment(content)
+    }
+
+    function handleLikeComment() {
+        //setLikeCount(likeCount +1)
+        setLikeCount((state) => {
+            return state + 1
+        });
     }
     return (
         <div className={styles.Comment}>
@@ -30,9 +39,9 @@ export function Comment({content, onDeleteComment}) {
                 </div>
 
                 <footer>
-                   <button>
+                   <button onClick={handleLikeComment}>
                     <ThumbsUp />
-                    Aplaudir <span>20</span>
+                    Aplaudir <span>{likeCount}</span>
                    </button>
                 </footer>
 
